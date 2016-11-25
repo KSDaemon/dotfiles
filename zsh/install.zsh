afb6a224f30d978bed5b6fcb0dbb5f35043d08f0
Konstantin Burkalev <KSDaemon@ya.ru>
2016-11-25 13:48:51 +0300

    Commented out antigen brew* bundles

--- zsh/install.zsh
+++ zsh/install.zsh
@@ -3,8 +3,8 @@
 source "$DOTFILES_ROOT/antigen/antigen.zsh"
 antigen use oh-my-zsh
 antigen bundle zsh-users/zsh-syntax-highlighting
-antigen bundle brew-cask
-antigen bundle brew
+#antigen bundle brew-cask
+#antigen bundle brew
 antigen bundle npm
 antigen bundle grunt
 antigen bundle symfony2
