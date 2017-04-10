b8a4fe53602a0a39b76e609556242ea1abc425b1
Konstantin Burkalev <KSDaemon@ya.ru>
2017-04-10 23:50:13 +0300

    Added some brew completions

--- homebrew/completion.zsh
+++ homebrew/completion.zsh
@@ -5,3 +5,17 @@ then
    source $completion
 fi
 
+completion='$(brew --prefix)/share/zsh/site-functions/_brew_cask'
+
+if test -f $completion
+then
+   source $completion
+fi
+
+completion='$(brew --prefix)/share/zsh/site-functions/_brew_services'
+
+if test -f $completion
+then
+   source $completion
+fi
+
