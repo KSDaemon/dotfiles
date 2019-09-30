884096177ef3869e3ca57a5bb03ed9cf9929a3fc
Konstantin Burkalev <KSDaemon@ya.ru>
2019-09-30 20:45:29 +0300

    Updated zsh git prompt python3 bin seaching to be working on linux/mac

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -10,7 +10,7 @@ ZSH_THEME_GIT_PROMPT_CHANGED="%{$fg_bold[blue]%}%{✚%G%}"
 ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}%{✔%G%}%{$reset_color%}"
 ZSH_THEME_GIT_PROMPT_BEHIND="%{%{$fg[cyan]%}↓%{$reset_color%}%G%}"
 ZSH_THEME_GIT_PROMPT_AHEAD="%{%{$fg[cyan]%}↑%{$reset_color%}%G%}"
-ZSH_GIT_PROMPT_PYBIN="/usr/local/bin/python3"
+ZSH_GIT_PROMPT_PYBIN="`which python3`"
 
 PROMPT='%B%{%F{green}%}%n%{%F{red}%}@%b%{%F{green}%}%m%B%{%F{green}%}:%{%F{magenta}%}%~/$(git_super_status)%(?.%B%{%F{green}%}.%{%F{red}%}!)%(!.#.>)%{$reset_color%} '
 RPROMPT='%B%{%F{green}%}[%{%F{cyan}%}%T%{%F{green}%}] %{%F{blue}%}cmd#%(?.%B%{%F{green}%}.%{%F{red}%})%h%{$reset_color%}'
