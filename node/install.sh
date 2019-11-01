54de9de915b625fec2d5c678d9b3e42499f34a4f
Konstantin Burkalev <KSDaemon@ya.ru>
2019-11-02 00:05:43 +0200

    Added some js frameworks to std install list

--- node/install.sh
+++ node/install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-npm install -g eslint mocha npm-check sails svgo webpack webpack-dev-server
+npm install -g eslint mocha npm-check sails svgo webpack webpack-dev-server @angular/cli @vue/cli typescript
