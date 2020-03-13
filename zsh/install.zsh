075fc3e6c77d19e5a0eb1363e061430df8b51e67
Konstantin Burkalev <KSDaemon@ya.ru>
2020-03-14 00:12:35 +0200

    Removed obsolete in zsh antigen bundles

--- zsh/install.zsh
+++ zsh/install.zsh
@@ -6,7 +6,4 @@ antigen bundle zsh-users/zsh-syntax-highlighting
 #antigen bundle brew-cask
 #antigen bundle brew
 antigen bundle npm
-antigen bundle grunt
-antigen bundle symfony2
-#antigen bundle zsh-users/zsh-completions src
 antigen apply
