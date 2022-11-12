34b0b84f363d59879a219e8240a86d86f42555de
Konstantin Burkalev <KSDaemon@gmail.com>
2022-11-12 13:19:09 +0200

    Added pnpm to node install

--- node/install.sh
+++ node/install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-npm install -g eslint mocha npm-check sails svgo webpack webpack-dev-server @vue/cli typescript
+npm install -g eslint mocha npm-check sails svgo webpack webpack-dev-server @vue/cli typescript pnpm
