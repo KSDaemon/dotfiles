b3edc3a6d78b3a5d84eaaf5b6ea1a0af6c806908
Konstantin Burkalev <KSDaemon@gmail.com>
2025-01-29 00:44:25 +0200

    Add alias docker=podman

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -43,6 +43,7 @@ alias lua5.2='rlwrap lua5.2'
 alias lua5.3='rlwrap lua5.3'
 alias clicolors='for i in {0..255}; do print -Pn "%K{$i}  %k%F{$i}${(l:3::0:)i}%f " ${${(M)$((i%20)):#3}:+"\n"}; done'
 alias removehuskyhooks='rm `grep -l husky .git/hooks/*`'
+alias docker=podman
 
 alias -g N='2>/dev/null'
 alias -g L='| less'
